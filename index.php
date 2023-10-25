4073c10b47490044ab37d77dac08c9bfff9f522e
Valentin-Lefort <lefortvalentin94@gmail.com>
2023-10-25 16:07:38 +0200

    addsession

--- index.php
+++ index.php
@@ -6,7 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Nique shoes</title>
 </head>
+<style>
+  img {
+    width: 100px;
+    height: 100px;
 
+  }
+</style>
 
 <body>
   <?php include 'header-display.php' ?>
