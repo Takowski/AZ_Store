2e45ee9a0e1c53f6c3be1703642aba43fdd2fad5
Takowski <93830775+Takowski@users.noreply.github.com>
2023-10-25 10:28:25 +0200

    add products page

---
+++ products.php
@@ -0,0 +1,27 @@
+<?php
+$items = [
+    [
+        'id' => 1,
+        'product' => 'NIQUE Air',
+        'price' => 234,
+        'image_url' => './assets/img/shoe_one.png',
+    ],
+    [
+        'id' => 2,
+        'product' => 'NIQUE Air',
+        'price' => 234,
+        'image_url' => './assets/img/shoe_one.png',
+    ],
+    [
+        'id' => 3,
+        'product' => 'NIQUE Air',
+        'price' => 234,
+        'image_url' => './assets/img/shoe_one.png',
+    ],
+    [
+        'id' => 4,
+        'product' => 'NIQUE Air',
+        'price' => 234,
+        'image_url' => './assets/img/shoe_one.png',
+    ],
+];
