4dbf4ef3bc721183e1a0009517227f7564d0e190
Valentin-Lefort <lefortvalentin94@gmail.com>
2023-10-25 10:52:27 +0200

    V1

---
+++ shopping-cart.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Shopping cart of NIQUE</title>
+</head>
+<body>
+  <?php include 'header-display.php' ?>
+  <main>
+
+  </main>
+
+</body>
+</html>
