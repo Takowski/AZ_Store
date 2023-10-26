a8ddde6d200bb14b1b3587603302592023b3f2ce
Justin Michel <justinmichel2000@gmail.com>
2023-10-26 13:24:55 +0200

    test

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Nique shoes</title>
+  <link rel="stylesheet" href="assets/css/style.css" type="text/css"/>
+
 </head>
 <!-- Style temporaire -->
 <style>
