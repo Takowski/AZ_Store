562d5b8240ed53b9c200b08dad053faf6dbacfbb
Takowski <93830775+Takowski@users.noreply.github.com>
2023-10-26 15:58:18 +0200

    stash

--- index.php
+++ index.php
@@ -22,6 +22,10 @@
     <?php
     session_start();
 
+    if (!isset($_SESSION['cart'])) {
+      $_SESSION['cart'] = [];
+    }
+
     $json_data = file_get_contents('assets/json/catalog.json');
 
 
@@ -40,13 +44,25 @@
           echo '<h2>' . $item['id'] . '</h2>';
           echo '<p>' . $item['product'] . '</p>';
           echo '<p>Price: $' . $item['price'] . '</p>';
-          echo '<div id="add-to-cart-' . $item['id'] . '">';
-          echo '<button type="submit" name="add_item" value="' . $item['id'] . '">Add to cart</button>';
+          echo '<form method="post">';
+          echo '<input type="hidden" name="item_id" value="' . $item['id'] . '">';
+          echo '<button type="submit" name="add_item" value="Add to cart">Add to cart</button>';
+          echo '</form>';
           echo '<hr>';
+          echo '<pre>' . print_r($_SESSION) . '</pre>';
         }
       }
     };
+
     ?>
+<?php
+if (isset($_POST['add_item'])) {
+  $item_id = $_POST['item_id'];
+  if (!in_array($item_id, $_SESSION['cart'])) {
+    $_SESSION['cart'][] = $item_id;
+  }
+}
+?>
   </main>
   <picture>
     <img src="assets/style/img/shoe_two.png" alt=" a blurple shoe">
