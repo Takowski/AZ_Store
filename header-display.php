f55e98e1bdb9dd127bd05de9e78c588d6d70fe59
Valentin-Lefort <lefortvalentin94@gmail.com>
2023-10-27 12:14:45 +0200

    array number calculted from shopping for index for the number of number

--- header-display.php
+++ header-display.php
@@ -14,4 +14,10 @@ echo '<header>
 <a href="shopping-cart.php"><img src="./assets/style/img/shopping-cart.svg"></a>
 </header>';
 
-echo '<span>' . array_sum(array_column($_SESSION['cart'], 'number')) . '</span>'; ?>
+$cartTotal = array_sum(array_column($_SESSION['cart'], 'number'));
+if ($cartTotal > 0) {
+  echo '<span>' . $cartTotal . '</span>';
+} else {
+  echo '<span style="display:none;"></span>';
+}
+?>
