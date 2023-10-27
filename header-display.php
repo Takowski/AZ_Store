177273089c4f3285d6719d707e65fca102ffe6f9
Takowski <93830775+Takowski@users.noreply.github.com>
2023-10-27 16:25:43 +0200

    suppression start session exept header

--- header-display.php
+++ header-display.php
@@ -14,9 +14,13 @@ echo '<header>
 <a href="shopping-cart.php"><img src="./assets/style/img/shopping-cart.svg"></a>
 </header>';
 
-$cartTotal = array_sum(array_column($_SESSION['cart'], 'number'));
-if ($cartTotal > 0) {
-  echo '<span>' . $cartTotal . '</span>';
+if (isset($_SESSION['cart'])) {
+  $cartTotal = array_sum(array_column($_SESSION['cart'], 'number'));
+  if ($cartTotal > 0) {
+    echo '<span>' . $cartTotal . '</span>';
+  } else {
+    echo '<span style="display:none;"></span>';
+  }
 } else {
   echo '<span style="display:none;"></span>';
 }
